0f107d0278743b9283c9a51232149d650585f7f2
furkaner <furkan08_@hotmail.com>
2022-04-18 16:42:22 +0300

    all states moved to context from question component

--- src/contexts/scoreContext.js
+++ src/contexts/scoreContext.js
@@ -1,88 +1,120 @@
 import React, { useContext, useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 const ScoreContext = React.createContext();
 
 const ScoreProvider = ({ children }) => {
-  const [score, setScore] = useState(0);
-  const [tour, setTour] = useState(1);
-  const [questionList, setQuestionList] = useState([]);
-  const [totalResults, setTotalResults] = useState([{
-    totalScore: 0,
-    totalQuestions: 0,
-    correctAnswers: 0,
-  }]);
-  const [soruSayisi, setSoruSayisi] = useState(1);
+  const [question, setQuestion] = useState('');
+  const [allQuestions, setAllQuestions] = useState([]);
+  const [correctAnswer,setCorrectAnswer] = useState(0);
+  const [correctAnswersNumber, setCorrectAnswersNumber] = useState(0);
+  const [incorrectAnswer,setIncorrectAnswer] = useState([]);
+  const [score,setScore] = useState(0);
+  const [tour,setTour] = useState(1);
+  const [isCorrect, setIsCorrect] = useState(null);
+  const [answers, setAnswers] = useState([]);
+  const navigate = useNavigate();
 
-  let totalScore=0, totalQuestions, correctAnswers;
  
+    const createQuestion = () => {
+      setIsCorrect(null);
+    let  randomNumber1 = Math.floor(Math.random() * 9)+1;
+    let  randomNumber2 = Math.floor(Math.random() * 9)+1;
+    setQuestion(`${randomNumber1} x ${randomNumber2}`);
+    setCorrectAnswer( randomNumber1 * randomNumber2);
+     setIncorrectAnswer([randomNumber2 * (randomNumber1 + 1), randomNumber1 * (randomNumber2 - 1)]);
+    let allOptions = [randomNumber1 * randomNumber2, randomNumber2 * (randomNumber1 + 1), randomNumber1 * (randomNumber2 - 1)];
+    let shuffledOptions = allOptions.sort(() => Math.random() - 0.5);
+    setAnswers(shuffledOptions);
+    };
 
-  useEffect(() => {
-    totalScore += score;
-    totalQuestions = 10;
-    correctAnswers = questionList.filter(
-      (item) => item.isCorrect === true
-    ).length;
-    
-    setTotalResults([
-      {
-        totalScore: totalScore,
-        totalQuestions: totalQuestions,
-        correctAnswers: correctAnswers,
+let checkAnswer
+    if (isCorrect === null) {
+     checkAnswer = (answer) => {
+      if (answer === correctAnswer) {
+        setIsCorrect(true);
+        setScore((prev) => prev + Math.ceil(Math.sqrt(correctAnswer)));
+        setAllQuestions(  [...allQuestions, { question:question, correct:correctAnswer, isCorrect:true }]);
+        setCorrectAnswersNumber(correctAnswersNumber + 1);
+        setTimeout(() => {
+          createQuestion();
+          setIsCorrect(null);
+        }, 3000);
+      } if (answer !== correctAnswer) {
+        setIsCorrect(false);
+        setAllQuestions(  [...allQuestions, { question:question, correct:correctAnswer, isCorrect:false }]);
+        setTimeout(() => {
+          createQuestion();
+          setIsCorrect(null);
+        }, 3000);
       }
-    ]);
-   
-  }, [soruSayisi,score]);
-   
- 
-
-
- 
+    };
+  }
   useEffect(() => {
-   
-    const total = JSON.parse(localStorage.getItem("total"));
-    console.log("girdi");
-    console.log(total);
-    if (!total) {
-      console.log("girdi");
-      localStorage.setItem("total", JSON.stringify([
-        {
-          totalScore: 0,
-          totalQuestions: 0,
-          correctAnswers: 0,
-        }
-      ]));
-    } else {
-      if(soruSayisi >10){
-      const newTotalResults = [{
-       
-        totalScore:totalResults[0].totalScore+parseInt(total[0].totalScore || 0),
-        totalQuestions:totalResults[0].totalQuestions+parseInt(total[0].totalQuestions || 0),
-        correctAnswers: totalResults[0].correctAnswers+parseInt(total[0].correctAnswers || 0),
-      }]
-      
-      localStorage.setItem("total", JSON.stringify(newTotalResults));
-      console.log(newTotalResults);
+    if (allQuestions.length === 10) {
+      setTimeout(() => {
+        navigate('/results');
+      }, 2500);
+
+      if (localStorage.getItem('totalScore')) {
+        const pointsFromStorage = +localStorage.getItem('totalScore');
+        localStorage.setItem('totalScore', pointsFromStorage + +score);
+      } else {
+        localStorage.setItem('totalScore', score);
+      }
+
+      if (localStorage.getItem('totalQuestions')) {
+        const questionsFromStorage = +localStorage.getItem('totalQuestions');
+        localStorage.setItem(
+          'totalQuestions',
+          questionsFromStorage + allQuestions.length
+        );
+      } else {
+        localStorage.setItem('totalQuestions', allQuestions.length);
+      }
+
+      if (localStorage.getItem('totalCorrectAnswers')) {
+        const correctAnswersFromStorage = +localStorage.getItem(
+          'totalCorrectAnswers'
+        );
+        localStorage.setItem(
+          'totalCorrectAnswers',
+          correctAnswersFromStorage + correctAnswersNumber
+        );
+      } else {
+        localStorage.setItem('totalCorrectAnswers', correctAnswersNumber);
+      }
     }
-    }
-    
-  }, [soruSayisi]);
+  }, [allQuestions]);
 
  
- 
-  
+
 
   return (
     <ScoreContext.Provider
       value={{
+        checkAnswer,
+        answers,
+        question,
+        correctAnswer,
+        incorrectAnswer,
         score,
-        setScore,
         tour,
+        setScore,
         setTour,
-        questionList,
-        setQuestionList,
-        soruSayisi,
-        setSoruSayisi,
-        totalResults,
+        allQuestions,
+        correctAnswersNumber,
+        createQuestion,
+        isCorrect,
+        setCorrectAnswer,
+        setCorrectAnswersNumber,
+        setIncorrectAnswer,
+        setQuestion,
+        setIsCorrect,
+        setAllQuestions,
+        
+        
       }}
     >
       {children}
